f8a698b497b20a56be5db45e2c2f693cbaa35004
krypton9208 <krypton9208@gmail.com>
2014-10-11 16:24:30 +0200

    Fizz Buzz game TDD tests

---
+++ FizzBuzz/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FizzBuzz
+{
+    public class Class1
+    {
+        [Fact]
+       public void PierwszyTest()
+        {
+            var game = new FizzBuzzGame();
+            var result = game.gra(1);
+            Assert.Equal(1, result);
+        }
+        
+        [Fact]
+        public void DrugiTest()
+        {
+            var game = new FizzBuzzGame();
+            var result = game.gra(45);
+            Assert.Equal("FizzBuzz", result);
+        }
+
+        [Fact]
+        public void TrzeciTest()
+        {
+            var game = new FizzBuzzGame();
+            var result = game.gra(4);
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void CzwartyTest()
+        {
+            var game = new FizzBuzzGame();
+            var result = game.gra(6);
+            Assert.Equal("Fizz", result);
+        }
+        [Fact]
+        public void PiatyTest()
+        {
+            var game = new FizzBuzzGame();
+            var result = game.gra(10);
+            Assert.Equal("Buzz", result);
+        }
+
+    }
+}
---
+++ FizzBuzz/FizzBuzzGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class FizzBuzzGame
+    {
+        
+        public object gra(int x)
+        {
+            
+            if (x % 3 == 0 && x % 5 != 0) return "Fizz";
+            else if (x % 5 == 0 && x % 3 != 0) return "Buzz";
+            else if (x % 3 == 0 && x % 3 == 0) return "FizzBuzz";
+            else return x;
+        }
+    }
+}
