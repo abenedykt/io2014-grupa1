b831ed4c5145a78146c0be88e01bca02ef3e0552
Siemienik Paweł <siemienik.pawel@gmail.com>
2014-10-11 16:03:14 +0200

    FizzBuzz test

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzGame
+    {
+        public object Play(int p)
+        {
+            if (p % 3 == 0)
+            {
+                if (p % 5 == 0)
+                {
+                    return "Fizz Buzz";
+                }
+                return "Fizz";
+            }
+            else if (p % 5 == 0)
+            {
+                return "Buzz";
+            }
+            return p.ToString();
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzzTest/FizzBuzzTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzz;
+
+namespace FizzBuzzTest
+{
+    [TestClass]
+    public class FizzBuzzTest
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            FizzBuzz = new FizzBuzzGame();
+        }
+        [TestMethod]
+        public void In1Out1Test()
+        {
+            Assert.AreEqual(FizzBuzz.Play(1), "1");
+        }
+        [TestMethod]
+        public void In3OutFizzTest()
+        {
+            Assert.AreEqual(FizzBuzz.Play(3), "Fizz");
+        }
+        [TestMethod]
+        public void In5OutBuzzTest()
+        {
+            Assert.AreEqual(FizzBuzz.Play(5), "Buzz");
+        }
+        [TestMethod]
+        public void In15OutFizzBuzzTest()
+        {
+            Assert.AreEqual(FizzBuzz.Play(15), "Fizz Buzz");
+        }
+        public FizzBuzzGame FizzBuzz { get; set; }
+    }
+}
