0bca4a0cea5fd7e4fc02c912fcf91525d839d374
coyot7 <grzestomiak@gmail.com>
2014-10-11 18:50:28 +0200

    Fizz Buzz z Konsola

---
+++ Fizz 3 Buzz 5 z Konsola/FizzBuzz/FizzBuzz/fissBassGame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzzGame
+{
+    public class fissBassGame
+    {
+        public object play(int p)
+        {
+            if (p % 3 == 0 && p % 5 != 0)
+            {
+                return "Fizz";
+            }
+            else if (p % 5 == 0 && p % 3 != 0)
+            {
+                return "Buzz";
+            }
+
+            else if (p % 5 == 0 && p % 3 == 0)
+            {
+                return "Fizz Buzz";
+            }
+            else
+            return p;
+        }
+    }
+}
---
+++ Fizz 3 Buzz 5 z Konsola/FizzBuzz/FizzBuzzGame/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FizzBuzz;
+
+namespace FizzBuzzGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbe calkowita: ");
+            var x = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            var gra = new fissBassGame();
+
+            Console.WriteLine(gra.play(x));
+            Console.ReadKey();
+        }
+    }
+}
