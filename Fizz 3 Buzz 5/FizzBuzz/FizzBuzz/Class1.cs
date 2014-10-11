373d771f92e6ecf724f58403c00c445c6a7f9158
coyot7 <grzestomiak@gmail.com>
2014-10-11 16:05:24 +0200

    FizzBuzz

---
+++ Fizz 3 Buzz 5/FizzBuzz/FizzBuzz/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FizzBuzz
+{
+    public class Class1
+    {
+        [Fact]
+        public void PierwszyTest()
+        {
+            Assert.Equal(true, true);
+        }
+
+        [Fact]
+        public void Sprawdzenie_Zwracanej_Liczby()
+        {
+            var game = new fissBassGame();
+            var result = game.play(1);
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Fizz()
+        {
+            var game = new fissBassGame();
+            var result = game.fizz(3);
+            Assert.Equal("Fizz", result);
+        }
+
+        [Fact]
+        public void Buzz()
+        {
+            var game = new fissBassGame();
+            var result = game.buzz(5);
+            Assert.Equal("Buzz", result);
+        }
+
+        [Fact]
+        public void FizzBuzz()
+        {
+            var game = new fissBassGame();
+            var result = game.fizzbuzz(15);
+            Assert.Equal("Fizz Buzz", result);
+        }
+    }
+}
---
+++ Fizz 3 Buzz 5/FizzBuzz/FizzBuzz/fissBassGame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz
+{
+    class fissBassGame
+    {
+        internal object play(int p)
+        {
+            return p;
+        }
+
+        internal object fizz(int p)
+        {
+            if (p % 3 == 0 && p % 5 != 0)
+            {
+                return "Fizz";
+            }
+            return 1;
+        }
+
+        internal object buzz(int p)
+        {
+            if (p % 5 == 0 && p % 3 != 0)
+            {
+                return "Buzz";
+            }
+            return 1;
+        }
+
+        internal object fizzbuzz(int p)
+        {
+            if (p % 5 == 0 && p % 3 == 0)
+            {
+                return "Fizz Buzz";
+            }
+            return 1;
+        }
+    }
+}
