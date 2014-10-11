4816392e57f5b1942f4923cab6266a803325c904
Szymon Wanot <szwanot@gmail.com>
2014-10-11 15:54:02 +0200

    FizzBuzz The Game

---
+++ Fizbuzz/Fizbuzz/FizzBuzz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fizbuzz
+{
+    public class FizzBuzz
+    {
+        public string Play(int number)
+        {
+            if (number % 5 == 0 && number % 3 == 0)
+                return "FizzBuzz";
+            else if (number % 3 == 0)
+                return "Fizz";
+            else if (number % 5 == 0)
+                return "Buzz";
+            else
+                return number.ToString();
+        }
+    }
+}
---
+++ Fizbuzz/FizbuzzTest/FizzBuzzTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fizbuzz;
+
+namespace FizbuzzTest
+{
+    [TestClass]
+    public class FizzBuzzTest
+    {
+        [TestMethod]
+        public void If_I_Put_1_I_Shoud_Get_1_Test()
+        { 
+            var game = new FizzBuzz();
+            var play = game.Play(1);
+            Assert.AreEqual("1", play);
+        }
+
+        [TestMethod]
+        public void If_I_Put_3_I_Shoud_Get_Fizz_Test()
+        {
+            var game = new FizzBuzz();
+            var play = game.Play(3);
+            Assert.AreEqual("Fizz", play);
+        }
+
+        [TestMethod]
+        public void If_I_Put_5_I_Shoud_Get_Fizz_Test()
+        {
+            var game = new FizzBuzz();
+            var play = game.Play(5);
+            Assert.AreEqual("Buzz", play);
+        }
+
+        [TestMethod]
+        public void If_I_Put_15_I_Shoud_Get_FizzBuzz_Test()
+        {
+            var game = new FizzBuzz();
+            var play = game.Play(15);
+            Assert.AreEqual("FizzBuzz", play);
+        }
+    }
+}
