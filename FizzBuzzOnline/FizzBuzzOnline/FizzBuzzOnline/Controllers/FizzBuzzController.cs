61c986f5af6974a93b3d0851464dcadd5e49c070
Michał Konieczny <mic22@mic22.info>
2014-10-27 15:21:52 -0700

    dodanie kontrolera

---
+++ FizzBuzzOnline/FizzBuzzOnline/FizzBuzzOnline/Controllers/FizzBuzzController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FizzBuzzOnline.Controllers
+{
+    public class FizzBuzzController : Controller
+    {
+        //
+        // GET: /FizzBuzz/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
