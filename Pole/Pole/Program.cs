9f9a35bd74dadfc0cb4f1a53127aa594357db86f
coyot7 <grzestomiak@gmail.com>
2014-11-15 16:22:58 +0100

    Pole kwadratu

---
+++ Pole/Pole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj bok a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj bok b: ");
+            int b = int.Parse(Console.ReadLine());
+            Kwadrat  kwadrat1 = new Kwadrat(a);
+            Console.WriteLine("Pole Kwadratu dla boku a wynosi: {0}", kwadrat1.Pole());
+            Console.WriteLine("Obwod Kwadratu dla boku a wynosi: {0}", kwadrat1.Obwod());
+            Prostokat prostoka1 = new Prostokat(a,b);
+            Console.WriteLine("Pole Prostokatu wynosi: {0}", prostoka1.Pole());
+            Console.WriteLine("Obwod Prostokatu wynosi: {0}", prostoka1.Obwod());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Pole/Pole/Prostokat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pole
+{
+    public class Prostokat
+    {
+        public int a {get; set;}
+        public int b { get; set; }
+
+        public Prostokat(int p1, int p2)
+        {
+             a = p1;
+             b = p2;
+        }
+
+        public double Pole()
+        {
+            return a * b;
+        }
+
+        public double Obwod()
+        {
+            return 2 * a + 2 * b;
+        }
+        
+    }
+}
