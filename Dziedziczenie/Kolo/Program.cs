ae1eb965a3d2f9e7468b6862e790ad2245d288bb
dkawczak <d@kawczak.pl>
2014-11-15 16:13:18 +0100

    Dziedziczenie
    Projekt Dziedzinczenie, prostokąt i kwadrat

---
+++ Dziedziczenie/Kolo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Xml;
+
+namespace Kolo
+{
+    class Square : Rectangle
+    {
+        public Square(int a) : base(a, a)
+        {
+        }
+    }
+    class Rectangle
+    {
+        private int b;
+        private int a;
+        public Rectangle(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public int Field()
+        {
+            return a * b;
+        }
+
+        public int Circuit()
+        {
+            return (2 * a) + (2 * b);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Dziedziczenie/Kolo/Tests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+
+namespace Kolo
+{
+    public class Tests
+    {
+        [Fact]
+        public void SprawdzPoleProstokat()
+        {
+            var rectangle = new Rectangle(2,5);
+            var result = rectangle.Field();
+            Assert.Equal(10, result);
+        }
+        [Fact]
+        public void SprawdzObwodProstokat()
+        {
+            var rectangle = new Rectangle(2, 5);
+            var result = rectangle.Circuit();
+            Assert.Equal(14, result);
+        }
+        [Fact]
+        public void SprawdzPoleKwadrat()
+        {
+            var square = new Square(5);
+            var result = square.Field();
+            Assert.Equal(25, result);
+        }
+        [Fact]
+        public void SprawdzObwodKwadrak()
+        {
+            var square = new Square(5);
+            var result = square.Circuit();
+            Assert.Equal(20, result);
+        }
+
+    }
+}
