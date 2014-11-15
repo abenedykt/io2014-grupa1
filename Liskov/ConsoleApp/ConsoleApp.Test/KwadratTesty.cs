020c7727bf710c7e7f8e42ef21433f644476bd91
psmyrdek <przemek.smyrdek@gmail.com>
2014-11-15 16:01:36 +0100

    prostokat testy

---
+++ Liskov/ConsoleApp/ConsoleApp.Test/KwadratTesty.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp;
+
+namespace ConsoleApp.Test
+{
+    [TestClass]
+    public class KwadratTesty
+    {
+        [TestMethod]
+        public void It_Should_Return_Correct_Square_Area()
+        {
+            FiguraAbstrakcyjna kwadrat = new Kwadrat(4.0);
+
+            double area = kwadrat.LiczPole();
+
+            Assert.IsTrue(area == 16.0);
+        }
+
+        [TestMethod]
+        public void It_Should_Return_Correct_Square_Length()
+        {
+            FiguraAbstrakcyjna kwadrat = new Kwadrat(4.0);
+
+            double length = kwadrat.LiczObwod();
+
+            Assert.IsTrue(length == 16.0);
+        }
+    }
+}
