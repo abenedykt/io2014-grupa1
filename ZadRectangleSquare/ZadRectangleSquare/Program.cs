5a4b85f99e6a2428a4077db58deb02290973b077
Lazys <adrian.bystrek@gmail.com>
2014-11-15 16:14:58 +0100

    ZadRectangleSquare

---
+++ ZadRectangleSquare/ZadRectangleSquare/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ZadRectangleSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(2, 3);
+
+            Console.WriteLine(rectangle.CalculateArea());
+            Console.WriteLine(rectangle.CalculateCircuit());
+
+            Square square = new Square(2);
+
+            Console.WriteLine(square.CalculateArea());
+            Console.WriteLine(square.CalculateCircuit());
+        }
+    }
+}
