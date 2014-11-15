4943b95db3b7c338d511c45afd7142a21a7c27ae
Michał Konieczny <mic22@mic22.info>
2014-11-15 06:40:38 -0800

    Rect field and langth

--- FigureField/FigureField/Program.cs
+++ FigureField/FigureField/Program.cs
@@ -6,10 +6,42 @@ using System.Threading.Tasks;
 
 namespace FigureField
 {
+    class Rectangle
+    {
+        protected double a, b;
+
+        public Rectangle(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public double GetField()
+        {
+            return a * b;
+        }
+
+        public double GetLength()
+        {
+            return (a * 2) + (b * 2);
+        }
+    }
+
+    class Square : Rectangle
+    {
+        public Square(double a) : base(a, a) { }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var rect = new Rectangle(5, 10);
+            Console.WriteLine(rect.GetField());
+            Console.WriteLine(rect.GetLength());
+
+            rect = new Square(10);
+            Console.WriteLine(rect.GetField());
+            Console.WriteLine(rect.GetLength());
         }
     }
 }
