9dd678687daa1a3e529e3230d924355c4c4134e7
ArturBaraszkiewicz <arturr.nn1@gmail.com>
2014-11-29 16:06:03 +0100

    proj

---
+++ UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+
+        public Produkt Pr { get; set; }
+
+        public void Init()
+        {
+            order = new Zam();
+
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+
+
+        }
+    }
+}
