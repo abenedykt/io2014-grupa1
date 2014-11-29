2f22070b307a75a9493ddac7350b5b25789d8fa0
Rafał <sailor53@wp.pl>
2014-11-29 16:23:01 +0100

    sdf

---
+++ rafał wandzel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wystawianie_faktur
+{
+
+
+
+    class produkt
+    {
+        int id { get; set; }
+        string nazwa { get; set; }
+
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float Cena = 800;
+            Console.WriteLine("Twoje zamowienie zawiera : okno");
+            Console.WriteLine("Wpisz vat ");
+            float Vat1 = float.Parse(Console.ReadLine());
+            float zamowienie = (Vat1/100)* Cena;
+            Console.WriteLine("Twoje zamówienie wynosi : {0}", zamowienie);
+
+        }
+
+
+               
+    }
+}
