014745eb40f7c40b8945a9c5965550e87ed39401
andrzejlajczak <andrzejlajczakath@gmail.com>
2014-12-04 15:45:04 +0100

    faktura

---
+++ main.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sprzedaz
+{
+
+    class Rabat
+    {
+        int rabat;
+        public int Rabat
+        {
+            
+            get { return rabat; }
+            set { rabat = value; }
+        }
+    }
+    class Vat
+    {
+        int vat;
+         public int Vat
+        {
+            get { return vat; }
+            set { vat = value; }
+        }
+    }
+    class Product
+    {
+       
+        public Product produkt { get; set; }
+
+        public int oblicz()
+        {
+
+        }
+
+        
+    }
+
+}
+
