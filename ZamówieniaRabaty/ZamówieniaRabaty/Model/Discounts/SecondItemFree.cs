c2e6c1db6b1713f99b397f89cf3eb1eae36ac29e
Lazys <adrian.bystrek@gmail.com>
2015-01-08 22:39:22 +0100

    Nowy typ promocji

---
+++ ZamówieniaRabaty/ZamówieniaRabaty/Model/Discounts/SecondItemFree.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ZamówieniaRabaty.Contract;
+
+namespace ZamówieniaRabaty.Model.Discounts
+{
+    public class SecondItemFree: IDiscount
+    {
+        public string Name { get; set; }
+
+        public void Calculate(IEnumerable<IItem> items)
+        {
+            items.OrderBy(x => x.Price).First().PriceAfterDiscount = 0;
+        }
+    }
+}
