7d07f0ae866769df347b87c60ef896a4687560bf
krypton9208 <krypton9208@gmail.com>
2015-01-24 16:41:12 +0100

    Próba używania aspektów

---
+++ aspekt/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult About()
+        {
+            return View( model: DateTime.Now.Millisecond.ToString() );
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
