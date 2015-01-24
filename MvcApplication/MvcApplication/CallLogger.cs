ea558e2f760d8fc77b760659304bcd6a2eb84ffc
coyot7 <grzestomiak@gmail.com>
2015-01-24 16:49:25 +0100

    licznik

---
+++ MvcApplication/MvcApplication/CallLogger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading;
+using System.Diagnostics;
+using Castle.DynamicProxy;
+using System.IO;
+
+namespace MvcApplication
+{
+    public class CallLogger : IInterceptor
+    {
+        TextWriter _output;
+
+        public CallLogger(TextWriter output)
+        {
+            _output = output;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            _output.Write("Calling method {0} with parameters {1}... ",
+              invocation.Method.Name,
+              string.Join(", ", invocation.Arguments.Select(a => (a ?? "").ToString()).ToArray()));
+
+            invocation.Proceed();
+
+            _output.WriteLine("Done: result was {0}.", invocation.ReturnValue);
+        }
+    }
+}
---
+++ MvcApplication/MvcApplication/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading;
+using System.Diagnostics;
+using Castle.DynamicProxy;
+using System.IO;
+
+namespace MvcApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Sekunda/
+
+        public ViewResult Index()
+        {
+            System.Threading.Thread.Sleep(2000);
+            
+            return View(model: (DateTime.Now.Second + DateTime.Now.Millisecond).ToString());
+        }
+
+    }
+}
