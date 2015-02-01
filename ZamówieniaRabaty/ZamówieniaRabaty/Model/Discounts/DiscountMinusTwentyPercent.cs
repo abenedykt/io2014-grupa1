ecb4c5d3cf02e66b83566bedb276194a763b8789
Lazys <adrian.bystrek@gmail.com>
2015-02-01 19:59:57 +0100

    Untracked files dodane

---
+++ ZamówieniaRabaty/ZamówieniaRabaty/Model/Discounts/DiscountMinusTwentyPercent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using ZamówieniaRabaty.Contract;
+
+namespace ZamówieniaRabaty.Model.Discounts
+{
+    public class DiscountMinusTwentyPercent : IDiscount
+    {
+        public string Name { get; set; }
+        public bool Combinable { get; set; }
+
+        public void Calculate(IEnumerable<IItem> items)
+        {
+            foreach (var item in items)
+            {
+                item.PriceAfterDiscount = item.Price - item.Price * 0.2;
+            }
+        }
+    }
+}
---
+++ ZamówieniaRabaty/ZamówieniaRabaty/Model/Items/Mouse.cs
@@ -0,0 +1,13 @@
+﻿using ZamówieniaRabaty.Contract;
+
+namespace ZamówieniaRabaty.Model.Items
+{
+    public class Mouse : IItem
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double PriceAfterDiscount { get; set; }
+
+        public IVat Vat { get; set; }
+    }
+}
