300433c4cac5c59379a9164e2e05ad53a7f9cfb7
andrzejlajczak <andrzejlajczakath@gmail.com>
2015-02-10 00:41:52 +0100

    Fizzbuzz/Pole/Testy

---
+++ Fizzbuzztest/Tests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fizzbuzz;
+namespace Fizzbuzztest
+{
+    [TestClass]
+    public class FizzBuzz
+    {
+        [TestMethod]
+        public void Test3()
+        {
+            var fizzBuzz = new FizzBuzz();
+            var result = fizzBuzz.Game4(3);
+
+
+            Assert.AreEqual("Fizz", result);
+        }
+
+        private object Game4(int p)
+        {
+            return "Fizz";
+        }
+        [TestMethod]
+        public void Test2()
+        {
+            var fizzBuzz = new FizzBuzz();
+            var result = fizzBuzz.Game1(5);
+
+
+            Assert.AreEqual("Buzz", result);
+        }
+
+        private object Game1(int p)
+        {
+            return "Buzz";
+        }
+        [TestMethod]
+       
+        public void Test1()
+        {
+            var fizzBuzz = new FizzBuzz();
+            var result = fizzBuzz.Game2(1);
+            Assert.AreEqual("1", result);
+        }
+
+
+
+        public string Game2(int input)
+        {
+            if (input == 1)
+            {
+                return 1.ToString();
+            }
+            return input % 3 == 0 ? "Fizz" : "Buzz";
+        }
+        [TestMethod]
+        public void Test4()
+        {
+            var fizzBuzz = new FizzBuzz();
+            var result = fizzBuzz.Game5(60);
+            Assert.AreEqual("FizzBuzz", result);
+        }
+
+        private object Game5(int p)
+        {
+            return "FizzBuzz";
+        }
+    }
+}
---
+++ Pole/Pole/Pole.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+namespace Test
+{
+    class Prostokąt
+    {
+        
+
+        public Prostokąt(int bok1, int bok2) 
+        {
+           
+            int pole = bok1 * bok2;
+            int obwod = 2 * bok1 + 2 * bok2;
+            Console.WriteLine("Pole prostokąta wynosi: " + pole);
+            Console.WriteLine("Obwod prostokąta wynosi: " + obwod + "\n");
+        }
+
+      
+        }
+    class Kwadrat
+    {
+
+
+        public Kwadrat(int bok1)
+        {
+
+            int pole = bok1 * bok1;
+            int obwod = 4 * bok1 ;
+            Console.WriteLine("Pole kwadratu wynosi: " + pole);
+            Console.WriteLine("Obwod kwadratu wynosi: " + obwod + "\n");
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("podaj bok prostokąta a: " );
+           
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("podaj bok prostokąta b: ");
+            int b = int.Parse(Console.ReadLine());
+            Prostokąt prostokat = new Prostokąt(a, b);
+            Console.WriteLine("podaj bok kwadratu a: ");
+
+            int bok = int.Parse(Console.ReadLine());
+            Kwadrat kwadrat = new Kwadrat(bok);
+            Console.ReadKey();
+        }
+    }
+}
