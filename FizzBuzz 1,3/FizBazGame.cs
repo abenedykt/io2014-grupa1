6d8456eb16c65e119e51cc1b8f4cf9325b948acd
ArturBaraszkiewicz <arturr.nn1@gmail.com>
2015-02-11 10:08:06 +0100

    FizBuzy

---
+++ FizzBuzz 1,3/FizBazGame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz
+{
+    class FizBazGame
+    {
+        internal object play(int p)
+        {
+            return p;
+        }
+
+        internal object fiz(int p)
+        {
+            if (p % 3 == 0 && p % 5 != 0)
+            {
+                return "Fiz";
+            }
+            return 1;
+        }
+
+        internal object buz(int p)
+        {
+            if (p % 5 == 0 && p % 3 != 0)
+            {
+                return "Buz";
+            }
+            return 1;
+        }
+
+        internal object fizbuz(int p)
+        {
+            if (p % 5 == 0 && p % 3 == 0)
+            {
+                return "FizBuz";
+            }
+            return 1;
+        }
+    }
+}
---
+++ FizzBuzz 1,3/TClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FizzBuzz
+{
+    public class TClass
+    {
+        [Fact]
+        public void TestJeden()
+        {
+            Assert.Equal(true, true);
+        }
+
+        [Fact]
+        public void SprRet()
+        {
+            var game = new FizBazGame();
+            var result = game.play(1);
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Fizz()
+        {
+            var game = new FizBazGame();
+            var result = game.fiz(3);
+            Assert.Equal("Fiz", result);
+        }
+
+        [Fact]
+        public void Buz()
+        {
+            var game = new FizBazGame();
+            var result = game.buz(5);
+            Assert.Equal("Buz", result);
+        }
+
+        [Fact]
+        public void FizzBuzz()
+        {
+            var game = new FizBazGame();
+            var result = game.fizbuz(15);
+            Assert.Equal("Fiz Buz", result);
+        }
+    }
+}
