9a1b3614cf08f49260f57aba7772dd7aebca2452
Artur <artur.hutyra@gmail.com>
2015-02-12 14:42:33 +0100

    FizzBuzzTest

---
+++ fizzbuzzTest/fizzbuzzTest/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace fizzbuzzTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(true, true);
+        }
+
+        [TestMethod]
+        public void Sprawdzenie_Zwracanej_Liczby()
+        {
+            var game = new fb();
+            var result = game.gra(1);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Fizz()
+        {
+            var game = new fb();
+            var result = game.gra(5);
+            Assert.AreEqual("Fizz", result);
+        }
+
+        [TestMethod]
+        public void Buzz()
+        {
+            var game = new fb();
+            var result = game.gra(3);
+            Assert.AreEqual("Buzz", result);
+        }
+
+        [TestMethod]
+        public void FizzBuzz()
+        {
+            var game = new fb();
+            var result = game.gra(15);
+            Assert.AreEqual("FizzBuzz", result);
+        }
+    }
+}
---
+++ fizzbuzzTest/fizzbuzzTest/fb.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzbuzzTest
+{
+    class fb
+    {
+        public object gra(int parametr)
+        {
+            for (int liczba = 0; liczba <= 100; liczba++)
+            {
+                if (liczba % 15 == 0)
+                {
+                    Console.WriteLine(liczba + "FizzBuzz");
+                }
+                else if (liczba % 5 == 0)
+                {
+                    Console.WriteLine(liczba + "Fizz");
+                }
+                else if (liczba % 3 == 0)
+                {
+                    Console.WriteLine(liczba + "Buzz");
+                }
+            }
+            return parametr;
+        }
+    }
+}
