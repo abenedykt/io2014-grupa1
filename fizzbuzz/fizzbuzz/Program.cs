7ab25ebd5790dc2adfb6fb57e8d902aebc93679b
Artur <artur.hutyra@gmail.com>
2015-02-12 14:42:24 +0100

    FizzBuzz

---
+++ fizzbuzz/fizzbuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int liczba = 0; liczba <= 100; liczba++)
+            {
+                if (liczba % 15 == 0)
+                {
+                    Console.WriteLine(liczba + "FizzBuzz");
+                }
+                else if (liczba % 5 == 0)
+                {
+                    Console.WriteLine(liczba + "Fizz");
+                }
+                else if (liczba % 3 == 0)
+                {
+                    Console.WriteLine(liczba + "Buzz");
+                }
+            }
+        }
+    }
+}
+
+
